581d30a9255c3118e3028abd6125c951ffd2b2ae
AlexCC1943 <alexcanar1943@gmail.com>
2025-07-15 11:32:09 -0500

    Updating a little text

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-	  res.send("¡Actualizado desde docker en GitHub Action y Desplegado en Azure!");
+	  res.send("¡Actualizado desde docker en GitHub Action y Desplegado en Azure! Ahora hay que ver como hacer esto mas automatico");
 });
 
 app.listen(port, () => {
